e1c2668acd56fbb5c650f3c4d56d556db7912ec3
novel14 <novel.palconit@module-zero.com>
2022-11-08 14:46:07 +0800

    Login Screen Layout

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+import "./Login.css";
+
+//IMG
+import logo from "../../Assets/Login/myt_logo.png";
+import username from "../../Assets/Login/username.png";
+import padlock from "../../Assets/Login/padlock1.png";
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className=''>
+      <div className='container'>
+        <Row className='pt-5'>
+          <Col>
+          <img src={logo} width={450} height={180}></img>
+          </Col>
+        </Row>
+        <Row className='center pt-5'>
+          <Col xs='5'>
+              <div className="input-group mb-3">
+                  <div className="input-group-prepend icon-cont">
+                      <span className="input-group-text center btn-login" >
+                          <img src={username} alt="branchcode" className="center img-icon"/> 
+                      </span>
+                  </div>
+                  <input type="text" className="form-control branch_code-input input-text" placeholder="Branch" aria-label="Branch" aria-describedby="basic-addon1"  />
+              </div>
+          </Col>
+        </Row>
+        <Row className='center'>
+          <Col xs='5'>
+              <div className="input-group mb-3">
+                  <div className="input-group-prepend icon-cont">
+                      <span className="input-group-text center btn-login" >
+                          <img src={padlock} alt="branchcode" className="img-icon"/> 
+                      </span>
+                  </div>
+                  <input type="text" className="form-control branch_code-input input-text" placeholder="Branch" aria-label="Branch" aria-describedby="basic-addon1"  />
+              </div>
+          </Col>
+        </Row>
+        <Row className='center'>
+          <Col xs='3'>
+            <button className='login-btn'>LOGIN</button>
+          </Col>
+        </Row>
+      </div>
+    </div>
   )
 }
 
