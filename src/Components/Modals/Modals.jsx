b9f588bf48c28306c764e744f92d8547c31f2b71
novel14 <novel.palconit@module-zero.com>
2022-12-05 16:33:18 +0800

    Responsiveness + modals

---
+++ src/Components/Modals/Modals.jsx
@@ -0,0 +1,234 @@
+import React, { useState, useRef } from "react";
+import { Row, Col } from "react-bootstrap";
+
+//Images
+import warning from "../../Assets/Modal/warning.png";
+
+//css
+import "../../Pages/Modal/Modal.css";
+
+
+const ModalPopUp = ({type,show,handleClose}) => {
+  const showHideClassName = show
+    ? "modal-pop display-block"
+    : "modal-pop display-none";
+
+
+  if (type === "delete") {
+    return (
+      <div className={showHideClassName}>
+        <section className="modal-mains modal-reset">
+          <div className="modal-cont-header d-flex justify-content-center">
+            <div className="modal-header-label">DELETE</div>
+          </div>
+          <hr className="modal-line" />
+          <div className="modal-content-body">
+            <Row className="">
+              <Row className='mt-4 mb-4'>
+                <label className="label">
+                  Are you sure want to delete ME?.
+                </label>
+              </Row>
+              <hr className="modal-line" />
+              <div className="mt-2 right d-flex justify-content-end">
+                  <button type="button" className="add-btn me-2">
+                    DELETE
+                  </button>
+                
+                <button type="button" className="cancel-btn ms-0" onClick = {handleClose}>
+                  CANCEL
+                </button>
+              </div>
+            </Row>
+          </div>
+        </section>
+      </div>
+    );
+  }
+
+  if (type === "warning") {
+    return (
+      <div className={showHideClassName}>
+        <section className="modal-mains modal-reset">
+          <div className="modal-cont-header d-flex justify-content-center">
+            <div className="modal-header-label">DELETE</div>
+          </div>
+          <hr className="modal-line" />
+          <div className="modal-content-body">
+            <Row className="">
+              <Row className='mt-4 mb-4 center'>
+                <img src={warning} className="warning-icon"></img>
+                <label className="label">
+                  Warning! You are not allowed to continue processing.
+                </label>
+              </Row>
+              <hr className="modal-line" />
+              <div className="mt-2 right d-flex justify-content-end">
+                <button type="button" className="cancel-btn ms-0" onClick = {handleClose}>
+                    CLOSE
+                </button>
+              </div>
+            </Row>
+          </div>
+        </section>
+      </div>
+    );
+  }
+
+  if (type === "edit") {
+    return (
+      <div className={showHideClassName}>
+        <section className="modal-mains modal-reset">
+          <div className="modal-cont-header d-flex justify-content-center">
+            <div className="modal-header-label">UPDATE</div>
+          </div>
+          <hr className="modal-line" />
+          <div className="modal-content-body">
+            <Row className="">
+              <Row className='mt-4 mb-4 center'>
+                <label className="label">
+                  ASA NAMAN ANG FORMS BAN HAHAHAH.
+                </label>
+              </Row>
+              <hr className="modal-line" />
+              <div className="mt-2 right d-flex justify-content-end">
+                <button type="button" className="add-btn me-2">
+                    EDIT
+                </button>
+                <button type="button" className="cancel-btn ms-0" onClick = {handleClose}>
+                    CLOSE
+                </button>
+              </div>
+            </Row>
+          </div>
+        </section>
+      </div>
+    );
+  }
+
+  if (type === "view") {
+    return (
+      <div className={showHideClassName}>
+        <section className="modal-mains modal-reset">
+          <div className="modal-cont-header d-flex justify-content-center">
+            <div className="modal-header-label">VIEW</div>
+          </div>
+          <hr className="modal-line" />
+          <div className="modal-content-body">
+            <Row className="">
+              <Row className='mt-4 mb-4 center'>
+                <label className="label">
+                  BEST FRIENDS CHECK!
+                </label>
+              </Row>
+              <Row className='mt- mb-4 center'>
+                <Col xs='5 left'>
+                <label className="label ms-5">
+                    NAME
+                </label>
+                </Col>
+                <Col xs='3 center'>
+                <label className="label">
+                    AGE
+                </label>
+                </Col>
+                <Col xs='4 center'>
+                <label className="label">
+                    STATUS
+                </label>
+                </Col>
+              </Row>
+              <Row className='mt- mb-4 center'>
+                <Col xs='5 left'>
+                <label className="label ms-5">
+                    Novel Palconit
+                </label>
+                </Col>
+                <Col xs='3 center'>
+                <label className="label">
+                   23
+                </label>
+                </Col>
+                <Col xs='4 center'>
+                <label className="label">
+                    Single
+                </label>
+                </Col>
+              </Row>
+              <Row className='mt- mb-4 center'>
+                <Col xs='5 left'>
+                <label className="label ms-5">
+                    Vanessa Ibale
+                </label>
+                </Col>
+                <Col xs='3 center'>
+                <label className="label">
+                   22
+                </label>
+                </Col>
+                <Col xs='4 center'>
+                <label className="label">
+                    Taken
+                </label>
+                </Col>
+              </Row>
+              <Row className='mt- mb-4 center'>
+                <Col xs='5 left'>
+                <label className="label ms-5">
+                    Donna Cuesta
+                </label>
+                </Col>
+                <Col xs='3 center'>
+                <label className="label">
+                   23
+                </label>
+                </Col>
+                <Col xs='4 center'>
+                <label className="label">
+                    Single
+                </label>
+                </Col>
+              </Row>
+              <Row className='mt- mb-4 center'>
+                <Col xs='5 left'>
+                <label className="label ms-5">
+                    Lalaine Labadan
+                </label>
+                </Col>
+                <Col xs='3 center'>
+                <label className="label">
+                   23
+                </label>
+                </Col>
+                <Col xs='4 center'>
+                <label className="label">
+                    Taken
+                </label>
+                </Col>
+              </Row>
+              <hr className="modal-line" />
+              <div className="mt-2 right d-flex justify-content-end">
+                <button type="button" className="cancel-btn ms-0" onClick = {handleClose}>
+                    CLOSE
+                </button>
+              </div>
+            </Row>
+          </div>
+        </section>
+      </div>
+    );
+  }
+
+  return (
+    <div className={showHideClassName}>
+      <section className="modal-main">
+        Here
+        <button type="button" onClick={handleClose}>
+          Close
+        </button>
+      </section>
+    </div>
+  );
+};
+
+export default ModalPopUp;
--- src/Pages/Modal/Modal.jsx
+++ src/Pages/Modal/Modal.jsx
@@ -1,18 +1,22 @@
 import React, {useState} from 'react'
 import { Row, Col } from 'react-bootstrap';
 
-//CSS
-import '../Reports/Report.css';
-
-//Images
-import Search from "../../Assets/Dashboard/search.png";
-
 //Component
 import Navbar from "../../Components/Navbar/Navbar";
-import Table from "../../Components/Table/Table";
-function Modal() {
+import ModalPopUp from '../../Components/Modals/Modals';
+
+//CSS
+import "./Modal.css";
+
+function Chart() {
 
     const [inactive, setInactive] = useState(false);
+
+    //Show modals
+    const [showModalDelete, setShowModalDelete] = useState(false)
+    const [showModalWarning, setShowModalWarning] = useState(false)
+    const [showModalEdit, setShowModalEdit] = useState(false)
+    const [showModalView, setShowModalView] = useState(false)
 
   return (
     <div className='page'>
@@ -20,38 +24,69 @@ function Modal() {
             onCollapse={(inactive) => {
             setInactive(inactive)
             }}
-            active={'REPORT'} 
+            active={'MANAGE'} 
         />
-        <div className={`page-container ${inactive ? "inactive" : "active"}`}>
+        <div className={`container ${inactive ? "inactive" : "active"}`}>
         {/* First Row */}
             <Row>
-                <Col xs='3 pt-2'>
+                <Col xs='6'>
                     <h1 className='page-title left'>MODAL</h1>
-                </Col>
-                <Col xs='5'>
-                    <div className='branch-wrapper right pt-3'>
-                        <input 
-                            type="text" 
-                            name="name" 
-                            placeholder='Search' 
-                            className='search-input-text'                    
-                        />
-                        <div className="branch-icon">
-                            <img className='search-icon' src={Search}></img>
-                        </div>
-                    </div>
-                </Col>
-                <Col xs='4 right'>
-                    <button className='add-btn me-4'>ADD</button>
-                    <button className='add-btn me-4'>PRINT</button>
-                    <button className='add-btn'>DOWNLOAD CSV</button>
                 </Col>
             </Row>
             <hr className='hr-line'/>
-            
+            <Row>
+                <Col xs="5">
+                    <label className='text-label'>Open Modal Delete</label>
+                    <Row>
+                        <button className='add-btn' onClick= {() => setShowModalDelete(true)}>DELETE</button>
+                    </Row>
+                </Col>
+                <Col xs="2"></Col>
+                <Col xs="5">
+                    <label className='text-label'>Open Modal Warning</label>
+                    <Row>
+                        <button className='add-btn' onClick= {() => setShowModalWarning(true)}>WARNING</button>
+                    </Row>
+                </Col>
+            </Row> 
+            <Row>
+                <Col xs="5">
+                    <label className='text-label'>Open Modal For Edit </label>
+                    <Row>
+                        <button className='add-btn' onClick= {() => setShowModalEdit(true)}>EDIT</button>
+                    </Row>
+                </Col>
+                <Col xs="2"></Col>
+                <Col xs="5">
+                    <label className='text-label'>Open Modal For View</label>
+                    <Row>
+                        <button className='add-btn' onClick= {() => setShowModalView(true)}>VIEW</button>
+                    </Row>
+                </Col>
+            </Row> 
         </div>
+        <ModalPopUp
+            type = "delete"
+            show = {showModalDelete}
+            handleClose = {() => setShowModalDelete(false)}
+        />
+        <ModalPopUp
+            type = "warning"
+            show = {showModalWarning}
+            handleClose = {() => setShowModalWarning(false)}
+        />
+        <ModalPopUp
+            type = "edit"
+            show = {showModalEdit}
+            handleClose = {() => setShowModalEdit(false)}
+        />
+        <ModalPopUp
+            type = "view"
+            show = {showModalView}
+            handleClose = {() => setShowModalView(false)}
+        />
     </div>
   )
 }
 
-export default Modal;
+export default Chart;
